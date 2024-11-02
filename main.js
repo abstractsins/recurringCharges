7bb2626e7b6badbe272961932ecf9f986ca42f98
Daniel Berlin <daniel.berlin@barcoding.com>
2024-11-02 01:52:14 -0400

    explicit recurring charges, objectification

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const pdf = require('pdf-parse');
+const { createBrotliDecompress } = require('zlib');
 
 var recurring = {
     explicit: [],
@@ -7,28 +8,190 @@ var recurring = {
     verySimilar: []
 }
 
-// read each file in dir and store as one text?
-const dir = '';
-let dataBuffer = fs.readFileSync('C:/Users/DanielBerlin/OneDrive - Barcoding, Inc/ACTUAL STUFF/trash/eStmt_2024-05-28.pdf');
+var allStatements = [];
 
-pdf(dataBuffer).then(function(data) {
-    parsingFunc(data.text);
-    console.dir(recurring, { depth: null, maxArrayLength: null });
+// read each file in dir and store as one text?
+const dir = 'D:/Projects/recurringCharges/statements/';
+
+fs.readdir(dir, (err, files) => {    
+    if (err) {
+        console.error("Could not list the directory.", err);
+        process.exit(1);
+    } else {
+        files.forEach((filename, index) => {
+            // ! for smaller source data
+            // if (index === 0) {
+                var file = dir + filename;
+                let dataBuffer = fs.readFileSync(file);
+                pdf(dataBuffer).then(function(data) {
+                    var debits = takeDebits(data.text);
+                    var entries = takeEntries(debits);
+                    entries = formatEntries(entries);
+                    var statement = objectifier(entries);
+                    allStatements.push(...statement);
+                }).then(function(){
+                    allStatements.sort(sortByDate);
+                    // console.dir(allStatements, { depth: null, maxArrayLength: null });
+                    var statements = allStatements.slice();
+                    statements = pullExplicitRecurring(statements);
+                    console.dir(recurring, { depth: null, maxArrayLength: null });
+                    console.dir(statements, { depth: null, maxArrayLength: null });
+                });
+            // }
+        });
+    }    
 });
 
-function parsingFunc(text) {
-    var lines = text.split('\n');
-    lines.forEach((line, i, a) => {
-        if (line.toLowerCase().match(/recurring/)) {
-            if (line.toLowerCase() === 'recurring') {
-                var fullEntry = [a[i-1], a[i], a[i+1]].join(' ');
-                recurring.explicit.push(fullEntry);
+
+function sortByDate(a, b) {
+    var dateA = a.date;
+    var monthA = parseInt(dateA.slice(0,2));
+    var dayA = parseInt(dateA.slice(3,5));
+    var yearA = parseInt(dateA.slice(-2));
+  
+    var dateB = b.date;
+    var monthB = parseInt(dateB.slice(0,2));
+    var dayB = parseInt(dateB.slice(3,5));
+    var yearB = parseInt(dateB.slice(-2));
+
+    if (dateA === dateB) {
+        return 0;
+    } else if (monthA === monthB) {
+        if (dayA === dayB) {
+            if (yearA < yearB) {
+                return -1;
             }
+        } else if (dayA < dayB) {
+            return -1;
+        } else if (dayA > dayB) {
+            return 1;
         }
-    });
+    } else if (monthA < monthB) {
+        return -1
+    } else if (monthA > monthB) {
+        return 1;
+    }
 }
 
-// look for word 'recurring'
-// look for precise repetition of amount
-// look for very similar repetition of amount
-// look for repeat vendors
+function takeDebits(string) {
+    var debitPhrase = 'total deposits and other additions';
+    var debitStart = string.toLowerCase().indexOf(debitPhrase);
+    var debits = string.substring(debitStart);
+    return debits;
+}
+
+function takeEntries(string) {
+    var reg = /\x0A(\d{2}\/\d{2}\/\d{2}.+)/;
+    var entries = string.split(reg);
+    entries.forEach((entry, i, a) => {
+        if (entry[0] === '\n') {
+            a[i-1] = a[i-1] + entry;
+            a[i] = '';
+        }
+    });
+    return entries;
+}
+
+function formatEntries(string) {
+    var entries = string.map(entry => {
+        entry = lineRemovals(entry);
+        entry = dateSeparator(entry);
+        entry = gapReplacer(entry);
+        entry = debitSpacer(entry);
+        entry = prologueRemover(entry);
+        return entry;
+    });
+
+    return entries;
+}
+
+function gapReplacer(entry) {
+    entry = entry.replaceAll(/\s{4,}/g, '    '); 
+    return entry;
+}
+
+function dateSeparator(entry) {
+    var reg = /(\d{2}\/\d{2}\/\d{2})(\w)/;
+    entry = entry.replace(reg, '$1'+' '+'$2');
+    return entry;
+}
+
+function lineRemovals(entry) {
+    entry = entry.replaceAll('\n', ' ');
+    return entry;
+}
+
+function emptyEntryRemovals(entry) {
+    if (typeof entry === 'object') {
+        entry = entry.filter((el, i) => {
+            if (typeof el === 'string') {
+                return el.length
+            }
+            return entry;
+        })
+    };
+    return entry;
+}
+
+function debitSpacer(entry) {
+    var amountReg = /(\-)(\d{1,}\.\d{2})/;
+    entry = entry.replace(amountReg, ' $1' + '$2');
+    return entry;   
+}
+    
+function prologueRemover(entry) {
+    var amountReg = /(\-)(\d{1,}\.\d{2})/;
+    var debit = entry.match(amountReg);   
+    if (debit) {
+        entry = entry.slice(0, debit['index']+debit[0].length+1);
+    }
+    return entry;
+}
+
+function objectifier(entries) {
+    entries = entries.map(entry => {
+        var date = entry.match(/\d{2}\/\d{2}\/\d{2}/);
+        var amount = entry.match(/\-\d{1,}(,\d{3})?\.\d{2}/);
+        if (date && amount) {
+            var desc = entry.slice(date[0].length+1, entry.length-[amount[0].length+1]);
+            // console.log([date[0], desc, amount[0]]);
+            var obj = {
+                date: date[0],
+                desc: desc.trim(), 
+                amount: amount[0]
+            }
+            return obj;
+        } else {
+            return '';
+        }
+    });
+
+    entries = emptyEntryRemovals(entries);
+ 
+    return entries;
+}
+
+function removeEntry(arr, index) {
+    // arr.splice(index, 1);
+    arr[index] = '';
+}
+
+
+
+
+
+
+
+
+
+function pullExplicitRecurring(statements) {
+    statements.forEach((entry,i) => {
+        if (entry.desc.toLowerCase().match(/recurring/)) {
+            recurring.explicit.push(entry);
+            removeEntry(statements, i);
+        }
+    });
+    statements = emptyEntryRemovals(statements);
+    
+    return statements;
+}
